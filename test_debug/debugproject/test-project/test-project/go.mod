8d241a9961008943f7bc5a89a9ec1401c65ef759
Omar Flores <dev@dev.com>
2025-07-25 08:08:23 -0700

    chore: remove test project files and directories

--- test_debug/debugproject/test-project/test-project/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/test/debugproject
-
-go 1.21
-
-require (
-	github.com/gorilla/mux v1.8.0
-	github.com/lib/pq v1.10.9
-)
--- test_debug/debugproject/test-project/test-project/internal/domain/errors.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-import "errors"
-
-var (
-	ErrInvalidTestUserFeatureData  = errors.New("invalid testuserfeature data")
-	ErrInvalidTestUserFeatureName  = errors.New("testuserfeature name is invalid")
-	ErrInvalidTestUserFeatureEmail = errors.New("testuserfeature email is invalid")
-)
